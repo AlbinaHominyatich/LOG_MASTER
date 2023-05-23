4e11c7ce0b9a4b4208e04f700b504d922ad1d641
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-05-23 18:55:49 +0300

    запис в .log

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import logging #стандартна бібліотека для логування перебігу програми
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG,
+                    filename= "logs.log",
+                    filemode= 'w',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug("debug")
 logging.info("info")
 logging.error("погано написав програму error")
