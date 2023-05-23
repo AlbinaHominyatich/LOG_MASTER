8e7e2b0f57e07dda955375573189065ed8c10e9d
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-05-23 19:00:56 +0300

    запис в .log та винятки

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ logging.info("info")
 logging.error("погано написав програму error")
 logging.warning("warning")
 logging.critical("critical")
+try:
+    print(10/0)
+except Exception:
+    logging.exception("Помилка(")
